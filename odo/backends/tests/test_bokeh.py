a455c82671d57fe03eed60b7f0c9c2a6bf1164e6
liudengfeng <liu.dengfeng@hotmail.com>
2017-12-30 19:15:49 +0800

    修正测试案例

--- odo/backends/tests/test_bokeh.py
+++ odo/backends/tests/test_bokeh.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import pytest
+import numpy as np
 bokeh = pytest.importorskip('bokeh')
 
 from odo.backends.bokeh import convert, pd, ColumnDataSource
@@ -12,15 +13,14 @@ df = pd.DataFrame([[100, 'Alice'],
                    [300, 'Charlie']],
                   columns=['balance', 'name'])
 
-
+# pandas升级
 def test_convert_dataframe_to_cds():
     cds = convert(ColumnDataSource, df)
-    assert cds.data == {'name': ['Alice', 'Bob', 'Charlie'],
-                        'balance': [100, 200, 300]}
+    assert str(cds.data) == str({'balance': np.array([100, 200, 300], dtype=np.int64),
+                                 'name': np.array(['Alice', 'Bob', 'Charlie'], dtype=object)})
 
     df2 = convert(pd.DataFrame, cds)
     assert isinstance(df2, pd.DataFrame)
 
     tm.assert_frame_equal(df, df2)
-
 
